31e6caf1befbbeadb44cb35707b17b2e0484aef6
AnneFiger <annefiger@gmail.com>
2021-11-28 16:08:20 +0000

    changed all wav. files to previous import method through src/ folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import "./App.scss";
 import crushedClap from "./clap-crushed.wav";
+import clapSlapper from "./clap-slapper.wav";
+import cowbell from "./cowbell-808.wav";
+import crashAcoustic from "./crash-acoustic.wav";
+import hihatAcoustic from "./hihat-acoustic01.wav";
+import hihatAnalog from "./hihat-analog.wav";
+import hihatElectro from "./hihat-electro.wav";
+import kick from "./kick-808.wav";
+import percHollow from "./perc-hollow.wav";
+ 
 
 import React from "react";
 import Drumpad from "./Drumpad";
@@ -7,31 +16,31 @@ import Drumpad from "./Drumpad";
 const audioBank = [
   { src: crushedClap, id: "crushed clap", drumKey: "Q", keyCode: 81 },
 
-  { src: "/clap-slapper.wav", id: "clap slapper", drumKey: "W", keyCode: 87 },
+  { src: clapSlapper, id: "clap slapper", drumKey: "W", keyCode: 87 },
 
-  { src: "/cowbell-808.wav", id: "cowbell", drumKey: "E", keyCode: 69 },
+  { src: cowbell, id: "cowbell", drumKey: "E", keyCode: 69 },
 
   {
-    src: "/crash-acoustic.wav",
+    src: crashAcoustic,
     id: "acoustic crash",
     drumKey: "A",
     keyCode: 65,
   },
 
   {
-    src: "/hihat-acoustic01.wav",
+    src: hihatAcoustic,
     id: "acoustic hihat",
     drumKey: "S",
     keyCode: 83,
   },
 
-  { src: "/hihat-analog.wav", id: "analog hihat", drumKey: "D", keyCode: 68 },
+  { src: hihatAnalog, id: "analog hihat", drumKey: "D", keyCode: 68 },
 
-  { src: "/hihat-electro.wav", id: "electro hihat", drumKey: "Z", keyCode: 90 },
+  { src: hihatElectro, id: "electro hihat", drumKey: "Z", keyCode: 90 },
 
-  { src: "/kick-808.wav", id: "kick 808", drumKey: "X", keyCode: 88 },
+  { src: kick, id: "kick 808", drumKey: "X", keyCode: 88 },
 
-  { src: "/perc-hollow.wav", id: "hollow perc", drumKey: "C", keyCode: 67 },
+  { src: percHollow, id: "hollow perc", drumKey: "C", keyCode: 67 },
 ];
 
 function App() {
@@ -53,14 +62,14 @@ function App() {
             ))}
             <p>{displaySound}</p>
             <label>Volume</label>
-            <input 
-            type="range" 
-            step="0.01" 
-            id="volume" 
-            onChange={(e) => setVolume(e.target.value)} 
-            value={volume}
-            max="1"
-            min="0"
+            <input
+              type="range"
+              step="0.01"
+              id="volume"
+              onChange={(e) => setVolume(e.target.value)}
+              value={volume}
+              max="1"
+              min="0"
             />
           </div>
         </div>
